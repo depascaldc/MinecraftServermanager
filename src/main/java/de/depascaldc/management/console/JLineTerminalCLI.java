29b5257a3c72edf43b879b5107a02cc64aac317b
Pascal LastName <depascaldc@gmail.com>
2020-07-04 07:36:42 +0200

    adding command completion // removing mouseevents (forgot after testing XD)

--- src/main/java/de/depascaldc/management/console/JLineTerminalCLI.java
+++ src/main/java/de/depascaldc/management/console/JLineTerminalCLI.java
@@ -36,22 +36,20 @@ import java.util.function.Consumer;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jline.builtins.Options.HelpException;
-import org.jline.reader.Candidate;
 import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReader.Option;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.MaskingCallback;
-import org.jline.reader.ParsedLine;
 import org.jline.reader.Parser;
 import org.jline.reader.impl.completer.ArgumentCompleter;
-import org.jline.terminal.Cursor;
-import org.jline.terminal.MouseEvent;
+import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.terminal.Terminal.Signal;
+import org.jline.terminal.Terminal.SignalHandler;
 import org.jline.utils.AttributedString;
 import org.jline.utils.DiffHelper.Operation;
-import org.jline.utils.InfoCmp.Capability;
 
 import de.depascaldc.management.commands.ConsoleCommand;
 import de.depascaldc.management.logger.ConsoleColors;
@@ -79,16 +77,16 @@ public class JLineTerminalCLI {
 			for (int i = 0; i < 256; i++) {
 				map[i] = Operation.INSERT;
 			}
-			TerminalBuilder builder = TerminalBuilder.builder();
+			TerminalBuilder builder = TerminalBuilder.builder().nativeSignals(true);
 			terminal = builder.build();
-			completer = new ArgumentCompleter(new Completer() {
-				@Override
-				public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
-					candidates.add(new Candidate("stop"));
-					candidates.add(new Candidate("kill"));
-					candidates.add(new Candidate("push"));
-				}
-			});
+			List<String> cmds = new ArrayList<String>();
+			for(ConsoleCommand command : ServerManager.getCommandMap().getCommands().values()) {
+				cmds.add(command.getName());
+				if(command.getAliases().size() > 0)
+					for(String alias : command.getAliases())
+						cmds.add(alias);
+			}
+			completer =new ArgumentCompleter(new StringsCompleter(cmds));
 			reader = LineReaderBuilder.builder().terminal(terminal).completer(completer).parser(parser)
 					.variable(LineReader.SECONDARY_PROMPT_PATTERN, rightPrompt).variable(LineReader.INDENTATION, 2)
 					.option(Option.INSERT_BRACKET, true).build();
@@ -98,25 +96,6 @@ public class JLineTerminalCLI {
 				reader.callWidget(LineReader.REDISPLAY);
 				reader.getTerminal().writer().flush();
 			}, 1, 1, TimeUnit.SECONDS);
-			reader.setOpt(LineReader.Option.MOUSE);
-			reader.getWidgets().put(LineReader.CALLBACK_INIT, () -> {
-				terminal.trackMouse(Terminal.MouseTracking.Any);
-				return true;
-			});
-			reader.getWidgets().put(LineReader.MOUSE, () -> {
-				MouseEvent event = reader.readMouseEvent();
-				StringBuilder tsb = new StringBuilder();
-				Cursor cursor = terminal.getCursorPosition(c -> tsb.append((char) c));
-				reader.runMacro(tsb.toString());
-				String msg = "          " + event.toString();
-				int w = terminal.getWidth();
-				terminal.puts(Capability.cursor_address, 0, Math.max(0, w - msg.length()));
-				terminal.writer().append(msg);
-				terminal.puts(Capability.cursor_address, cursor.getY(), cursor.getX());
-				terminal.flush();
-				return true;
-			});
-			new JLTCLI_KeypressWidget(reader);
 			callbacks.forEach(c -> c.accept(reader));
 			if (!callbacks.isEmpty()) {
 				Thread.sleep(2000);
